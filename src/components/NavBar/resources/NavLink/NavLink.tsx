cce70feb4f3628e926bf571b42ed23b2bbb928e7
jhoselin ramirez <jhoselin.ramirez92@gmail.com>
2023-12-16 17:07:21 -0600

    station 16/12/23

--- src/components/NavBar/resources/NavLink/NavLink.tsx
+++ src/components/NavBar/resources/NavLink/NavLink.tsx
@@ -2,14 +2,20 @@ import Button from "../../../Button/Button"
 import type { NavLinkProps } from "./NavLink_types"
 
 function NavLink({ children }: NavLinkProps): JSX.Element {
-  return <li>
-    <Button className="!px-0 relative after:absolute after:w-full after:h-[2px] after:bg-accent-blue-400 after:left-0 after:top-full
-    after:scale-x-0 data-[hovered]:after:scale-x-100 after:transition-transform data-[pressed]:after:bg-accent-blue-600 data-[focus-visible]:after:scale-x-100 
-    data-[focus-visible]:!outline-none"
-     buttonType="transparent">
-    {children}
-    </Button>
+  return (
+    <li>
+      <Button
+        className="!px-0 w-full text-start data-[focus-visible]:!outline-none 
+        data-[hovered]:bg-tuatara-900 data-[focus-visible]:bg-tuatara-900
+        md:relative md:after:absolute md:after:w-full md:after:h-[2px] md:after:bg-accent-blue-400 md:after:left-0 
+        md:after:top-full md:after:scale-x-0 md:data-[hovered]:after:scale-x-100 md:after:transition-transform 
+        md:data-[pressed]:after:bg-accent-blue-600 md:data-[focus-visible]:after:scale-x-100"
+        buttonType="transparent"
+      >
+        {children}
+      </Button>
     </li>
+  )
 }
 
 export default NavLink
